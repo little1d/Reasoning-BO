1a480b7b0b080530f5ac1e3c8b9ae7d42e72bca5
little1d <2431542525@qq.com>
2025-03-05 15:02:25 +0800

    feat(bugfix): fix generate function

--- src/llms/deepseek.py
+++ src/llms/deepseek.py
@@ -11,24 +11,55 @@ class DeepSeekClient:
         self.client = OpenAI(
             api_key=config.DEEPSEEK_API_KEY, base_url=config.DEEPSEEK_API_BASE
         )
+        self.messages = []
         self.content = ""
         self.reasoning_content = ""
-        self.messages = []
 
     def generate(
         self,
         user_prompt: str,
+        # 设置的小一点，方便调试
         max_tokens: int = 4096,
     ) -> Tuple[str, str]:
-        """返回元组 (content, reasoning_content)"""
+        """返回元组 (content, reasoning_content) 非流式"""
+        # 追加用户消息到历史记录
         self.messages.append({"role": "user", "content": user_prompt})
-        print(self.messages)
+
+        response = self.client.chat.completions.create(
+            model=config.DEEPSEEK_MODEL_NAME,
+            messages=self.messages,
+            stream=False,
+            max_tokens=max_tokens,
+        )
+
+        self.content = response.choices[0].message.content
+        self.reasoning_content = response.choices[0].message.reasoning_content
+
+        self.messages.append(
+            {
+                "role": "assistant",
+                "content": response.choices[0].message.content,
+            }
+        )
+        return (self.content, self.reasoning_content)
+
+    def stream_generate(
+        self,
+        user_prompt: str,
+        max_tokens: int = 4096,
+    ):
+        """流式返回内容和推理内容"""
+        self.messages.append({"role": "user", "content": user_prompt})
+
         response = self.client.chat.completions.create(
             model=config.DEEPSEEK_MODEL_NAME,
             messages=self.messages,
             stream=True,
             max_tokens=max_tokens,
         )
+
+        self.content = ""
+        self.reasoning_content = ""
 
         for chunk in response:
             if chunk.choices[0].delta.reasoning_content:
@@ -38,5 +69,16 @@ class DeepSeekClient:
             else:
                 self.content += chunk.choices[0].delta.content
 
-            self.messages.append(response.choices[0].message)
+        self.messages.append(
+            {
+                "role": "assistant",
+                "content": response.choices[0].message.content,
+            }
+        )
+
         return (self.content, self.reasoning_content)
+
+    def view_messages(self):
+        """查看对话历史"""
+        for message in self.messages:
+            print(f"{message['role']}: {message['content']}")
